128a1ea7b29f3d2ad390d7c8958d4a3b6d06b172
Tanimara Elias Santos <tanimaraeliassantos@gmail.com>
2026-09-22 11:59:01 +0200

    chore(frontend): remove unused default module

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Upload } from './components/upload/upload';
 import { AdmissionResponse } from './models/admission.model';
-import { CommonModule } from '@angular/common';
 import { Ranking } from './components/ranking/ranking';
 
 @Component({
   selector: 'app-root',
-  imports: [Upload, Ranking, CommonModule],
+  imports: [Upload, Ranking],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
